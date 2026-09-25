b062435210cfb95901d95c3c1130a2ba6a13fadf
ayushkumar40-plac <vtu31916@veltech.edu.in>
2026-09-25 08:25:06 +0530

    Time: 3 ms (31.21%) | Memory: 10.4 MB (66.75%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        vector<int> output;
+        backtrack(nums, 0, output, result);
+        return result;
+    }
+    
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& output, vector<vector<int>>& result) {
+        result.push_back(output);
+        
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            
+            output.push_back(nums[i]);
+            backtrack(nums, i + 1,output, result);
+            output.pop_back(); 
+        }
+    }
+};
